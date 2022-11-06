1a2f9c7ae12431b7057fdca971543538b2e014d5
Kouichi_Takano <tx2bxn@icloud.com>
2022-11-06 16:25:00 +0900

    install gulp

---
+++ gulp/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const flexBugsFixies = require('postcss-flexbugs-fixes')
+const cssWring = require('csswring')
+
+const autoprefixerOption = {
+  grid: true
+}
+
+const postcssOption = [
+  flexBugsFixies,
+  autoprefixer(autoprefixerOption),
+  cssWring
+]
+
+gulp.task('sass', () => {
+  return gulp.src('../src/web/_assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(postcss(postcssOption))
+    .pipe(gulp.dest('../src/web/_assets/css'))
+});
+
+gulp.task('watch', () => {
+  return gulp.watch('../src/web/_assets/scss/**/*.scss', ['sass']);
+});
+
